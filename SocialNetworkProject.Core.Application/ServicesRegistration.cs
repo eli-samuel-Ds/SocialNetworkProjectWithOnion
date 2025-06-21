85c123229ca633a631f53afeb4bf923535ee5161
eli-samuel-Ds <elisamuelsuerorodriguez@gmail.com>
2025-06-21 00:49:00 -0400

    Add project files.

---
+++ SocialNetworkProject.Core.Application/ServicesRegistration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetworkProject.Core.Application
+{
+    internal class ServicesRegistration
+    {
+    }
+}
